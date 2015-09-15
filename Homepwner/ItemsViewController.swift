59a7025fa14735656dce36f304a1d188ab110738
jroland <jroland@linkedin.com>
2015-09-15 13:59:15 -0700

    Created dequeueable table cells

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class ItemsViewController: UITableViewController {
+class ItemsViewController: UITableViewController, UITableViewDataSource {
     
     let itemStore: ItemStore
     
     init(itemStore: ItemStore) {
         self.itemStore = itemStore
         super.init(nibName: nil, bundle: nil)
+        
+        for _ in 0..<5 {
+            self.itemStore.createItem()
+        }
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+    }
     required init!(coder aDecoder: NSCoder!) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK:
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell(style: .Default, reuseIdentifier: "UITableViewCell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as! UITableViewCell
+        //ReuseIdentifier described in detail on page 144
+        
+        let item = itemStore.allItems[indexPath.row]
+        cell.textLabel?.text = item.name
+        return cell
     }
     
     
