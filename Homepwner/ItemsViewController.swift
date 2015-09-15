20a900455414892f733906a5e14b067cc197e422
jroland <jroland@linkedin.com>
2015-09-15 14:20:16 -0700

    Added custom cells

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -23,8 +23,14 @@ class ItemsViewController: UITableViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        
+//        tableView.registerClass(UITableViewCell.self,
+//            forCellReuseIdentifier: "UITableViewCell")
+        let nib = UINib(nibName: "ItemCell", bundle: nil)
+        tableView.registerNib(nib, forCellReuseIdentifier: "ItemCell")
+        tableView.rowHeight = 44
     }
+    
     required init!(coder aDecoder: NSCoder!) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -37,11 +43,15 @@ class ItemsViewController: UITableViewController, UITableViewDataSource {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 //        let cell = UITableViewCell(style: .Default, reuseIdentifier: "UITableViewCell")
-        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as! UITableViewCell
+//        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("ItemCell", forIndexPath: indexPath) as! ItemCell
         //ReuseIdentifier described in detail on page 144
         
         let item = itemStore.allItems[indexPath.row]
-        cell.textLabel?.text = item.name
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
+        
         return cell
     }
     
