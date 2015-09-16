388c7b0868873e4f243bc43b42d465999825028e
jroland <jroland@linkedin.com>
2015-09-16 10:39:41 -0700

    Made Item NSCoding compliant

--- Homepwner/Item.swift
+++ Homepwner/Item.swift
@@ -8,17 +8,28 @@
 
 import UIKit
 
-class Item: NSObject {
+class Item: NSObject , NSCoding {
     var name: String
     var valueInDollars: Int
     var serialNumber: String?
     let dateCreated: NSDate
+    let itemKey: String
     
     init(name: String, serialNumber: String?, valueInDollars: Int) {
         self.name = name
         self.valueInDollars = valueInDollars
         self.serialNumber = serialNumber
         self.dateCreated = NSDate()
+        self.itemKey = NSUUID().UUIDString //Uniquely identifying string
+        super.init()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObjectForKey("name") as! String
+        dateCreated = aDecoder.decodeObjectForKey("dateCreated") as! NSDate
+        itemKey = aDecoder.decodeObjectForKey("itemKey") as! String
+        serialNumber = aDecoder.decodeObjectForKey("serialNumber") as! String?
+        valueInDollars = aDecoder.decodeIntegerForKey("valueInDollars")
         super.init()
     }
     
@@ -40,5 +51,21 @@ class Item: NSObject {
         else {
             self.init(name: "", serialNumber: nil, valueInDollars: 0)
         }
+    }
+    
+    //MARK - NSCoding Protocol
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: "name")
+        aCoder.encodeObject(dateCreated, forKey: "dateCreated")
+        aCoder.encodeObject(itemKey, forKey: "itemKey")
+        
+        if let serialNumber = serialNumber {
+            aCoder.encodeObject(serialNumber, forKey: "serialNumber")
+        }
+        
+        //int is not an object, so we can't encode that way.
+        //Check NSCoder Docs for this
+        aCoder.encodeInteger(valueInDollars, forKey: "valueInDollars")
     }
 }
