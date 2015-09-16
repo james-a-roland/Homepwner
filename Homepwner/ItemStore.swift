5a12dac6fde176696e981344170229a291d0f06d
jroland <jroland@linkedin.com>
2015-09-16 10:47:43 -0700

    Allow for loading of Items in KeyedArchiver

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -11,6 +11,14 @@ import UIKit
 class ItemStore: NSObject {
     var allItems: [Item] = []
     
+    let itemArchiveURL: NSURL = {
+        let documentsDirectories =
+        NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        //UserDomainMask is always used for iOS in particular.
+        let documentDirectory = documentsDirectories.first as! NSURL
+        return documentDirectory.URLByAppendingPathComponent("items.archive")
+    }()
+    
     func createItem() -> Item {
         let newItem = Item(random: true)
         allItems.append(newItem)
@@ -31,6 +39,10 @@ class ItemStore: NSObject {
         let movedItem = allItems[fromIndex]
         allItems.removeAtIndex(fromIndex)
         allItems.insert(movedItem, atIndex: toIndex)
+    }
+    
+    func saveChanges() -> Bool {
+        return NSKeyedArchiver.archiveRootObject(allItems, toFile: itemArchiveURL.path!)
     }
 }
 
